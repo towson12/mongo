8c131752d39960df6279663e8d50fb7fa8491d9b
Jeff S <jsmetona@gmail.com>
2016-07-19 22:13:45 -0400

    working on collection spec rq05

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,14 @@
 class Place
   include Mongoid::Document
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+  def initialize(params)
+  	@id = params[:_id]
+  	@formatted_address = params[:formatted_address]
+  	@location = params[:geometry[:geolocation]
+  	@address_components = params[:address_components]
+  end
+
 
   def mongo_client
   	Mongoid::Clients.default
